d97d2abe4e439914f40c6684dc60b760e7ed4b52
Jason Alojado <xstopoyx@gmail.com>
2026-09-05 15:24:15 +0800

    feat: add filter by type

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Layout from './layout/Layout';
 import HomePage from './pages/HomePage';
 import PokemonDetailsPage from './pages/PokemonDetailsPage';
+import SearchPage from './pages/SearchPage';
+import TypePage from './pages/TypePage';
 import { ErrorMessage } from './components/ui';
 
 // This is the list of pages in the app, and the address each one lives at.
@@ -13,6 +15,8 @@ export default function App() {
         <Route element={<Layout />}>
           <Route path="/" element={<HomePage />} />
           <Route path="/pokemon/:nameOrId" element={<PokemonDetailsPage />} /> 
+          <Route path="/search" element={<SearchPage />} />
+          <Route path="/type/:type" element={<TypePage />} />
 
           {/* "*" matches any address we did not list above. */}
           <Route
