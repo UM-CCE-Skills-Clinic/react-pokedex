936aaf1723f502ce6f70c022a81a39a4cc10e708
Cabz_Red <g.cabaya.518891@umindanao.edu.ph>
2026-09-05 03:15:16 -0400

    feat: add pokemon search

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Layout from './layout/Layout';
 import HomePage from './pages/HomePage';
 import { ErrorMessage } from './components/ui';
 import PokemonDetailsPage from './pages/PokemonDetailsPage';   // ← add this
+import SearchPage from './pages/SearchPage';                   // ← add this
 
 
 // This is the list of pages in the app, and the address each one lives at.
@@ -14,6 +15,7 @@ export default function App() {
         <Route element={<Layout />}>
           <Route path="/" element={<HomePage />} />
           <Route path="/pokemon/:nameOrId" element={<PokemonDetailsPage />} />   {/* ← add this */}
+          <Route path="/search" element={<SearchPage />} />     {/* ← add this */}
 
           {/* "*" matches any address we did not list above. */}
           <Route
