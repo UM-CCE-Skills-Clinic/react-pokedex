9372279b9304ba3c4bf238e153e9dbe2e36a52f2
jiyawz <jayarcalalo09@gmail.com>
2026-09-05 15:31:34 +0800

    style: set up tailwind theme and page shell

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import { useState } from 'react'
 import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <>  
       <section id="center">
         <div className="hero">
           <img src={heroImg} className="base" width="170" height="179" alt="" />
