c2bec622abcfc52e7c9e8365c4483a7477f7309d
sisaryuuu <c.mulato.517213@umindanao.edu.ph>
2026-09-05 14:51:40 +0800

    feat: add pokemon details page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Layout from './layout/Layout';
 import HomePage from './pages/Homepage';
+import PokemonDetailsPage from './pages/PokemonDetailsPage';
 import { ErrorMessage } from './components/ui';
 
 // This is the list of pages in the app, and the address each one lives at.
@@ -11,6 +12,7 @@ export default function App() {
         {/* Every page inside here is drawn inside Layout. */}
         <Route element={<Layout />}>
           <Route path="/" element={<HomePage />} />
+          <Route path="/pokemon/:nameOrId" element={<PokemonDetailsPage />} />
 
           {/* "*" matches any address we did not list above. */}
           <Route
