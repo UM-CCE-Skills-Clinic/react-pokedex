b83eb7a35bf25410e67d64da03da26b2d81cf1d8
jjustrrona <r.poliquit.518518@umindanao.edu.ph>
2026-09-05 15:56:58 +0800

    test: verify all tests and quality checks pass

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Layout from './layout/layout';
+import Layout from './layout/Layout';
 import HomePage from './pages/HomePage';
 import PokemonDetailsPage from './pages/PokemonDetailsPage';
 import SearchPage from './pages/SearchPage';
